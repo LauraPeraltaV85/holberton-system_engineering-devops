3efa23ac305e2e4d9cbd16f96ff37d5b1476a1d2
LauraPeraltaV85 <lalaperalta85@gmail.com>
2020-02-03 21:23:38 +0000

    [fix] indentation

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     ttask = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
                          .format(argv[1]))
     tot_n_tasks = ttask.json()
-with open('{}.csv'.format(argv[1]), mode='w') as csv_file:
+    with open('{}.csv'.format(argv[1]), mode='w') as csv_file:
         csv = csv.writer(csv_file, delimiter=',', quoting=csv.QUOTE_ALL)
         for task in tot_n_tasks:
             csv.writerow([task.get('userId'), em_name,
