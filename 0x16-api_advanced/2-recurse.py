8a07ef3c8f6e29e3d244a86212b12b6788655cd6
LauraPeraltaV85 <lalaperalta85@gmail.com>
2020-02-07 03:02:35 +0000

    [feature] recursively

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+hot post reursively
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=""):
+    headers = {'user-agent': 'laura_req'}
+    if (len(hot_list) == 0):
+        subr = requests.get("https://www.reddit.com/r/{}/hot.json"
+                            .format(subreddit), headers=headers)
+    else:
+        subr = requests.get("https://www.reddit.com/r/{}/hot.json?after={}"
+                            .format(subreddit, after), headers=headers)
+
+    if subr.status_code is 200:
+        subscribers = (subr.json().get('data').get('children'))
+        for hot in subscribers:
+            hot_list.append(hot.get('data').get('title'))
+    elif after is None:
+        return hot_list
+    else:
+        return(None)
+
+    after = (subr.json().get('data').get('after'))
+    return recurse(subreddit, hot_list, after)
